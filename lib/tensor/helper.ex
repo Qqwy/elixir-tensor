e8a54e7c4cf89970943e1c2adce488c7e37eb529
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2016-07-06 19:56:18 +0200

    Chess test matrices.

--- lib/tensor/helper.ex
+++ lib/tensor/helper.ex
@@ -13,7 +13,6 @@ defmodule Tensor.Helper do
     [1, 4, 3, 2, 5]
   """
   def swap_elems_in_list(list, pos_a, pos_b) do
-    #IO.inspect [list, pos_a, pos_b]
     a = Enum.fetch!(list, pos_a)
     b = Enum.fetch!(list, pos_b)
     
@@ -21,6 +20,17 @@ defmodule Tensor.Helper do
     |> List.replace_at(pos_a, b)
     |> List.replace_at(pos_b, a)
   end
+
+  def swap2(list, pos_a, pos_a), do: list 
+  def swap2(list, pos_a, pos_b) when pos_a < pos_b do
+    {initial, rest} = Enum.split(list, pos_a)
+    {between, tail} = Enum.split(rest, pos_b - pos_a)
+    a = hd(between)
+    b = hd(tail)
+    initial ++ [b] ++ tl(between) ++ [a] ++ tl(tail)
+  end
+
+  def swap2(list, pos_a, pos_b) when pos_b < pos_a, do: swap2(list, pos_b, pos_a)
 
   # MAPS
 
@@ -47,7 +57,6 @@ defmodule Tensor.Helper do
 
   defp do_put_in_path(map, [key | keys], val, acc) do
     new_acc = acc ++ [key]
-    #IO.inspect([map, key, keys, val, acc, new_acc])
     new_map = put_in(map, new_acc, get_in(map, new_acc) || %{})
     do_put_in_path(new_map, keys, val, new_acc)
   end

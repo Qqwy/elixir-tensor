d7bd48f2e6233268aac36546fe2dfebce89c4362
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2016-07-07 17:56:04 +0200

    Better list swap algorithm.

--- lib/tensor/helper.ex
+++ lib/tensor/helper.ex
@@ -12,17 +12,8 @@ defmodule Tensor.Helper do
     iex> swap_elems_in_list([1,2,3,4,5], 1, 3)
     [1, 4, 3, 2, 5]
   """
-  def swap_elems_in_list(list, pos_a, pos_b) do
-    a = Enum.fetch!(list, pos_a)
-    b = Enum.fetch!(list, pos_b)
-    
-    list
-    |> List.replace_at(pos_a, b)
-    |> List.replace_at(pos_b, a)
-  end
-
-  def swap2(list, pos_a, pos_a), do: list 
-  def swap2(list, pos_a, pos_b) when pos_a < pos_b do
+  def swap_elems_in_list(list, pos_a, pos_a), do: list 
+  def swap_elems_in_list(list, pos_a, pos_b) when pos_a < pos_b do
     {initial, rest} = Enum.split(list, pos_a)
     {between, tail} = Enum.split(rest, pos_b - pos_a)
     a = hd(between)
@@ -30,39 +21,8 @@ defmodule Tensor.Helper do
     initial ++ [b] ++ tl(between) ++ [a] ++ tl(tail)
   end
 
-  def swap2(list, pos_a, pos_b) when pos_b < pos_a, do: swap2(list, pos_b, pos_a)
+  def swap_elems_in_list(list, pos_a, pos_b) when pos_b < pos_a, do: swap_elems_in_list(list, pos_b, pos_a)
 
-  def map_swap(list, pos_a, pos_b) do
-    map = for {v, i} <- (list |> Enum.with_index), into: %{}, do: {i, v}
-    a = map[pos_a]
-    map
-    |> put_in([pos_a], map[pos_b])
-    |> put_in([pos_b], a)
-    |> Map.values
-  end
-
-  def map_swap_2(list, pos_a, pos_b) do
-    map = 
-      list
-      |> list_to_indexed_map
-    a = map[pos_a]
-    map
-    |> put_in([pos_a], map[pos_b])
-    |> put_in([pos_b], a)
-    |> Map.values
-  end
-
-  def list_to_indexed_map(list) do
-    do_list_to_indexed_map(list, 0, %{})
-  end
-
-  def do_list_to_indexed_map([], index, acc_map) do
-    acc_map
-  end
-
-  def do_list_to_indexed_map([head | tail], index, acc_map) do
-    do_list_to_indexed_map(tail, index+1, Map.put(acc_map, index, head))
-  end
 
   # MAPS
 
